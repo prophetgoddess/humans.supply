021040eee01b1d2c2c42dae6789e0b10558cc810
lauramichet <lauramichet@gmail.com>
2024-10-05 17:01:26 -0700

    Update Names.ts

--- src/lib/data/Names.ts
+++ src/lib/data/Names.ts
@@ -6,14 +6,113 @@ export interface Name {
 export const names = {
     ReproductionChamber: {
         singular: "Reproduction Chamber",
-        plural: "Reproduction Chambers"
+        plural: "Reproduction Chambers",
+        description: "Placing even-numbered groups of humans in this chamber generally results in some population growth. Occasionally, humans refuse to reproduce, and emerge from the chamber as rude, unmanageable oafs."
     },
     SolitaryConfinement: {
         singular: "Isolation Chamber",
-        plural: "Isolation Chambers"
+        plural: "Isolation Chambers",
+        description: "This chamber prevents human socialization and limits sensory input. We find that after some time in this environment, most humans become more obedient to our commands. (Regrettably, no human process has a perfect success rate.) Obedient humans improve the performance of the facility."
     },
     MeatGrinder: {
         singular: "Epicurean Hall",
-        plural: "Epicurean Halls"
-    }
+        plural: "Epicurean Halls",
+        description: "Gargelmaxian palates deserve a truly hearty meal. This facility produces various food ingredients approved by the GFDA: cooking oils, milled meats, calicum supplements, flavorings, and so on. The more piable the human stock, the higher quality the produce."
+    },
+    Gymnasium: {
+        singluar: "Exertion Facility".
+        plural: "Exertion Facilities",
+        description: "Observing humans, we noticed a tendency for physical exertion to take the form of oppositional, combative performance and ritual. This facility organizes humans into two teams, then exerts them against one another until they become stronger. Not all humans survive the process, and some humans become rude. Obedient humans are required to referee the match."
+    },
+    SensoryCrucible: {
+        singlular: "Sensory Crucible",
+        plural: "Sensory Crucibles",
+        description: "Sometimes, we need additional rude, disobedient humans for the creation of industrial solvents. These chambers shock humans repeatedly until their demeanor becomes unruly and their sentiment of us becomes highly negative. Obedient humans are required to operate the machinery."
+    },
+    GeneEditing: {
+        singluar: "Genetic Innovation Lab",
+        plural: "Genetic Innovation Labs",
+        description: "With the help of modern biotech, human genetics are easily modifiable. Many specialized humans can be improved through this process. Obedient humans are needed to corral and restrain the subjects. Not all genetic experiments are successful."
+    },
+    ObedienceSchool: {
+        singular: "Obedience School",
+        plural: "Obedience Schools",
+        description: "These schools can train obedient humans to truly love and trust the citizens of Gargelmax. Well-trained humans are highly rewarding pets, and can out-perform typical obedient humans at most tasks. Not all obedient humans pass the final exam, however."    
+    },
+    FeedFermenter: {
+        singular: "Feed Fermenter",
+        plural: "Feed Fermenters",
+        description: "Industrial scale meat-flakers and fermentation vats create a hearty, nutritionally complex human-offal byproduct. The process alters its texture and flavor so that other humans will consent to eat it."
+    },
+    CircusAcademy: {
+        singular: "Circus Academy",
+        plural: "Circus Academies",
+        description: "Humans make for an agreeable source of entertainment, if trained correctly. This facility transforms humans into jesters and seals them inside easily-shippable flatpack frames."
+    },
+    BonemealCompactor: {
+        singular: "Bonemeal Compactor",
+        plural: "Bonemeal Compactors",
+        description: "This compactor slurries and strains human biomatter. Industrial ovens bake the resulting feedstock into lightweight yet durable bricks, suitable for most construction projects."
+    },
+    BileHarvester: {
+        singular: "Bile Harvester",
+        plural: "Bile Harvesters",
+        description: "Rude humans are throught to produce stomach acid of a superior strength. This facility extracts the acid and refines it into an industrial solvent, ready for use in cleaning products."
+    },
+    NormalHumans: {
+        singular: "Human",
+        plural: "Humans",
+        description: "Unspecialized (or \"raw\") humans can perform most tasks at a low skill level. Simple processing can transform them into a variety of useful export commodities. Humans will eat one another as food, provided the material is put through a minimum level of industrial processing. Their mood is fickle, but can be kept in check by a wise manager."
+    },
+    ObedientHumans: {
+        singular: "Obedient Human",
+        plural: "Obedient Humans",
+        description: "This chamber prevents human socialization and limits sensory input. We find that after some time in this environment, most humans become more Gargelmax-aligned. (Regrettably, no human process has a perfect success rate.)"
+    },
+    RudeHumans: {
+        singular: "Rude Human",
+        plural: "Rude Humans",
+        description: "Humans are weak and unreliable. Most industrial processes create unacceptable quantities of waste humans - too unreliable, aggressive, and rebellious to manage. Their digestive acids can be used in the creation of industrial solvents."
+    },
+    HeartyHumans: {
+        singular: "Hearty Human",
+        plural: "Hearty Humans",
+        description: "With sufficient training, most humans can build hypertrophic muscle mass. Though this transformation has a high failure rate, the resultant humans can be used as building materials."
+    },
+    LigneousHumans: {
+        singular: "Ligneous Human",
+        plural: "Ligneous Humans",
+        description: "Hearty humans can withstand aggressive genetic alteration. Ligneous humans have a knobbly, highly-resilient exterior carapace which can be used as a raw material in major construction projects."
+    },
+    BonelessHumans: {
+        single: "Boneless Human",
+        plural: "Boneless Humans",
+        description: "These humans have been genetically altered to grow to adulthood without bones. Most must be kept in fluid-filled sacs for their own protection--though some are able to locomote using stub appendages."
+    },
+    AlienLovingHuman: {
+        single: "Gargelmax-Loving Human",
+        plural: "Gargelmax-Loving Humans",
+        description: "Some obedient humans are suitable for further training. These wise, trustworthy Gargelmax-loving humans have fully accepted their place in Gargelmax society, and can be trusted with more complex responsibilities."
+    },
+    HumanFeed: {
+        single: "Human Feed",
+        plural: "Human Feed",
+        description: "Human muscle is passed through a meat flaker and fermented. The resulting slurry's taste and texture is sufficient for consumption by other humans."
+    },
+    IndividualMood: {
+        single: "Mood",
+        plural: "Mood",
+        description: "The mood of this human."
+    },
+    PopulationMood: {
+        single: "Mood",
+        plural: "Mood",
+        description: "The overall mood of the human population."
+    },
+    Money: {
+        single: "Gargacoin",
+        plural: "Gargacoins",
+        description: "The legal tender of the planet Gargelmax."
+    },
+    
 }
