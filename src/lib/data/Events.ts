a48e23a7e7431ac44760dfd1e51eba611fc46ab4
cass <prophet_goddess@protonmail.com>
2024-10-05 17:31:33 -0700

    events data model

---
+++ src/lib/data/Events.ts
@@ -0,0 +1,9 @@
+export interface Event {
+    text: string
+}
+
+export const names = {
+    human_repro_1: {
+        text: "Congratulations, Facility Manager. You have turned two humans into three. Surely your old professors at the Gargelmax Xenoagriculture School will be pleased with your ingenuity."
+    }
+}
