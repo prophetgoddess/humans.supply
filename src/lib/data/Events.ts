b238e9443b21422c1511b5a40f9b6a318f2aef94
lauramichet <lauramichet@gmail.com>
2024-10-05 17:48:27 -0700

    Update Events.ts

--- src/lib/data/Events.ts
+++ src/lib/data/Events.ts
@@ -5,5 +5,36 @@ export interface Event {
 export const names = {
     human_repro_1: {
         text: "Congratulations, Facility Manager. You have turned two humans into three. Surely your old professors at the Gargelmax Xenoagriculture School will be pleased with your ingenuity."
+    },
+    rude_human_1: {
+        text: "One of your humans has become rude and rebellious. They are using their opposable thumbs and oppositional attitude to scurry about the facility, causing problems and defacing facility property. Deal with them immediately!"
+    },
+    rude_human_2: {
+        text: "The number of rude, rebellious humans infesting your facility has grown. Your superiors are displeased."
+    },
+    rude_human_3: {
+        text: "Humans are so small that it is difficult to even grasp them with your grabber-limb. As a result, they evade most conventional efforts to control them."
+    },
+    rude_human_4: {
+        text: "You have so many rude humans in your facility that they have created their own society in the rafters. They evade glue traps with ease and rain small pellets of waste down on your facility operators."
+    },
+    rude_human_5: {
+        text: "The rude humans remain a stain on your reputation. At department meetings, the other Factory Managers whisper about your poor performance."
+    },
+    rude_human_6: {
+        text: "Sometimes you think that the rude humans are making each other more rude. They are whispering, but their voices are too small and high-pitched for you to understand what they are saying."
+    },
+    rude_human_7: {
+        text: "The rude humans are painting tiny pictures of you - of YOU! - on the walls. Very close to the floor. You have to bend down to see them."
+    },
+    rude_human_8: {
+        text: "You were sleeping last night and a rude human crawled inside your ear and stabbed you with a small "knife" made out of a flake of aluminum. The facility must ban canned beverages!"
+    },
+    rude_human_9: {
+        text: "There seems to be nothing you can to do control the behavior of rude humans. They are constantly climbing your lower limbs and gouging your skin with weapons. Mostly, you no longer feel safe to enter the facility."
+    },
+    rude_human_10: {
+        text: "Your doctor says that you need to take time off to recover from \"burnout.\" You are too worried to take time off because you are afraid that the humans will ruin everything while you are gone."
     }
+    
 }
